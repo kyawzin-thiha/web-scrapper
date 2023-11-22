ff2a5de0025f2868832da7e42836cef94b6ec56a
Kyaw Zin Thiha <github@kyawzinthiha.dev>
2023-11-22 23:52:54 +0000

    api setup

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import type { ConfigService as ConfigServiceType } from "@nestjs/config";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import helmet from "helmet";
+import * as cookieParser from "cookie-parser";
+import * as compression from "compression";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = app.get<ConfigServiceType>(ConfigService);
+
+  app.enableCors({
+    origin: config.get("CORS_ORIGIN") || "http://localhost:3000",
+    credentials: true
+  });
+
+  app.use(helmet());
+  app.use(cookieParser(config.get("COOKIE_SECRET")));
+  app.use(compression());
+
+  const PORT = config.get("PORT") || 3001;
+  await app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }
+
 bootstrap();
